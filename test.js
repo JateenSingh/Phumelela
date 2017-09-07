62260b86dec4fdd0e9ab283c372d1aedbd7ff08a
JateenSingh <jitz303@gmail.com>
2017-09-07 11:56:38 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2
+// @description  try to take over the world!
+// @author       Jateen Singh
+// @match        http://tampermonkey.net/index.php?version=4.4&ext=dhdg&updated=true
+// @grant        none
+// @updateURL    https://drive.google.com/open?id=0B7J5gG71QSK9ejFsRDRFYU9DZGc
+// @downloadURL  https://drive.google.com/open?id=0B7J5gG71QSK9ejFsRDRFYU9DZGc
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	alert("I am an alert box!");
+    // Your code here...
+})();
